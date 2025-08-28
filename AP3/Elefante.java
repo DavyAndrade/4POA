fe147c7914f29cd9e70f5bf627e5e6e9ac38c48b
DavyAndrade <davy.dama55@gmail.com>
2025-08-28 15:53:44 -0300

    feat: AP3
    Serialização de Elefante

---
+++ AP3/Elefante.java
@@ -0,0 +1,140 @@
+package AP3;
+
+import java.io.Serializable;
+
+public class Elefante implements Serializable {
+    private String nome;
+    private int idade;
+    private String especie;
+    private double peso;
+    private double altura;
+    private String cor;
+    private String sexo;
+    private String localDeOrigem;
+    private String dataNascimento;
+    private boolean possuiPresas;
+    private double tamanhoOrelha;
+    private String estadoSaude;
+
+    public Elefante(String nome, int idade, String especie, double peso, double altura, String cor, String sexo,
+            String localDeOrigem, String dataNascimento, boolean possuiPresas, double tamanhoOrelha,
+            String estadoSaude) {
+        this.nome = nome;
+        this.idade = idade;
+        this.especie = especie;
+        this.peso = peso;
+        this.altura = altura;
+        this.cor = cor;
+        this.sexo = sexo;
+        this.localDeOrigem = localDeOrigem;
+        this.dataNascimento = dataNascimento;
+        this.possuiPresas = possuiPresas;
+        this.tamanhoOrelha = tamanhoOrelha;
+        this.estadoSaude = estadoSaude;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getLocalDeOrigem() {
+        return localDeOrigem;
+    }
+
+    public void setLocalDeOrigem(String localDeOrigem) {
+        this.localDeOrigem = localDeOrigem;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public boolean isPossuiPresas() {
+        return possuiPresas;
+    }
+
+    public void setPossuiPresas(boolean possuiPresas) {
+        this.possuiPresas = possuiPresas;
+    }
+
+    public double getTamanhoOrelha() {
+        return tamanhoOrelha;
+    }
+
+    public void setTamanhoOrelha(double tamanhoOrelha) {
+        this.tamanhoOrelha = tamanhoOrelha;
+    }
+
+    public String getEstadoSaude() {
+        return estadoSaude;
+    }
+
+    public void setEstadoSaude(String estadoSaude) {
+        this.estadoSaude = estadoSaude;
+    }
+
+    @Override
+    public String toString() {
+        return "Elefante [nome=" + nome + ", idade=" + idade + ", especie=" + especie + ", peso=" + peso + ", altura="
+                + altura + ", cor=" + cor + ", sexo=" + sexo + ", localDeOrigem=" + localDeOrigem + ", dataNascimento="
+                + dataNascimento + ", possuiPresas=" + possuiPresas + ", tamanhoOrelha=" + tamanhoOrelha
+                + ", estadoSaude=" + estadoSaude + "]";
+    }
+
+}
---
+++ AP3/Main.java
@@ -0,0 +1,41 @@
+package AP3;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        Elefante e1 = new Elefante("Dumbo", 10, "Africano", 5000.0, 3.0, "Cinza", "Macho", "África", "2014-05-20", true,
+                1.5, "Saudável");
+
+        try (FileOutputStream arqSer = new FileOutputStream("c:/arquivo/elefante.ser")) {
+            ObjectOutputStream oSer = new ObjectOutputStream(arqSer);
+            oSer.writeObject(e1);
+            oSer.close();
+            arqSer.close();
+
+            e1.setNome("Dumbo Jr.");
+            System.out.println(e1);
+            System.out.println("-".repeat(20));
+
+            FileInputStream arqDes = new FileInputStream("c:/arquivo/elefante.ser");
+            ObjectInputStream oDes = new ObjectInputStream(arqDes);
+
+            e1 = (Elefante) oDes.readObject();
+
+            oDes.close();
+            arqDes.close();
+
+            System.out.println("-".repeat(20));
+            System.out.println(e1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
